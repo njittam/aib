6ee8a97380ae87ecf5edbbb5dc6b5d02e3d3aebf
njittam <mattijn.k@gmail.com>
2015-04-16 16:31:54 +0200

    opdracht 1 en 2 klaar

--- task1/1-2-python/buyLotsOfFruit.py
+++ task1/1-2-python/buyLotsOfFruit.py
@@ -19,27 +19,31 @@ Cost of [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)] is 12.25
 import sys
 import re
 
-fruitPrices = {'apples':2.00, 'oranges': 1.50, 'pears': 1.75,
-              'limes':0.75, 'strawberries':1.00}
+fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
+               'limes': 0.75, 'strawberries': 1.00}
+
 
 def buyLotsOfFruit(orderList):
     """
         orderList: List of (fruit, numPounds) tuples
             
     Returns cost of order
-    """ 
-    totalCost = 0.0             
-    "*** YOUR CODE HERE ***"
+    """
+    totalCost = 0.0
+    for item in orderList:
+        if item[0] in fruitPrices:    totalCost += fruitPrices[item[0]] * item[1]
+        else:
+            return None
     return totalCost
-    
+
 # Main Method    
 if __name__ == '__main__':
     "This code runs when you invoke the script from the command line"
-    orderList = [ ('apples', 2.0), ('pears', 3.0), ('limes', 4.0) ]
+    orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
     print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
     if len(sys.argv) == 1:
-      args=input("Enter any command line arguments?")
+        args = input("Enter any command line arguments?")
     if args != '':
-      sys.argv.extend(re.split(r' *',args))
+        sys.argv.extend(re.split(r' *', args))
     print(str(len(sys.argv)) + ': ')
     print(sys.argv)
--- test.py
+++ test.py
@@ -1,5 +1,3 @@
 test = 2+3
 test2 = 42
 print (test + test2)
-
-
