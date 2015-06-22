e46779e14e51f50d1cabc034e158e1e267d4b37e
mattijn <m.kreuzen@student.ru.nl>
2015-06-23 01:00:55 +0200

    meer pep8

--- task7/6-contest/competitionAgents.py
+++ task7/6-contest/competitionAgents.py
@@ -8,37 +8,51 @@
 
 from util import manhattanDistance, nearestPoint
 from game import Directions, Agent
-import random, util
+import random
+import util
 import distanceCalculator
 import math
-class Corridor():
-    def __init__(self,layout):
+class SeekerClassifier:
+    def __init__(self):
+        util.raiseNotDefined()
+
+
+class Corridors:
+    def __init__(self, layout):
         self.layout = layout
 
-    def get_corridor(self,layout):
+    def compute_corridors(self):
         # print (layout.walls.data)
+        layout_data = self.layout.walls.data
+        explored = []
+        for h in range(layout.height):
+            for w in range(layout.width):
+                if layout_data[h][w]:
+                    explored.append((h,w))
+                else:
+                    legal_actions()
         return
 
 class CompetitionAgent(Agent):
-  """
-  A base class for competition agents.  The convenience methods herein handle
-  some of the complications of the game.
-
-  Recommended Usage:  Subclass CompetitionAgent and override getAction.
-  """
-
-  #############################
-  # Methods to store key info #
-  #############################
-
-  def __init__( self, index=0, timeForComputing = .1 ):
     """
-    Lists several variables you can query:
-    self.index = index for this agent
-    self.distancer = distance calculator (contest code provides this)
-    self.timeForComputing = an amount of time to give each turn for computing maze distances
-        (part of the provided distance calculator)
+    A base class for competition agents.  The convenience methods herein handle
+    some of the complications of the game.
+
+    Recommended Usage:  Subclass CompetitionAgent and override getAction.
     """
+
+    #############################
+    # Methods to store key info #
+    #############################
+
+    def __init__( self, index=0, timeForComputing = .1 ):
+      """
+      Lists several variables you can query:
+      self.index = index for this agent
+      self.distancer = distance calculator (contest code provides this)
+      self.timeForComputing = an amount of time to give each turn for computing maze distances
+          (part of the provided distance calculator)
+      """
     # Agent index for querying state, N.B. pacman is always agent 0
     self.index = index
 
@@ -53,118 +67,118 @@ class CompetitionAgent(Agent):
 
     # useful function to find functions you've defined elsewhere..
     # self.usefulFunction = util.lookup(usefulFn, globals())
+    self.getDistance = self.distancer.getDistance
+    self.next_food = None
+    self.food_list = []
 
+    def registerInitialState(self, game_state):
+        """
+        This method handles the initial setup of the
+        agent to populate useful fields.
 
-  def registerInitialState(self, gameState):
-    """
-    This method handles the initial setup of the
-    agent to populate useful fields.
+        A distanceCalculator instance caches the maze distances
+        between each pair of positions, so your agents can use:
+        self.distancer.getDistance(p1, p2)
+        """
+        self.distancer = distanceCalculator.Distancer(game_state.data.layout)
+        self.distancer.getMazeDistances()
+        #get_tunnels(game_state.data.layout)
+        food = game_state.getFood()
+        self.food_list = []
+        for h in range(food.height):
+            for w in range(food.width):
+                if food[w][h]:
+                    self.food_list.append((w, h))
+        foods = [(self.distancer.getDistance(game_state.getPacmanPosition(), food),food) for food in self.food_list]
+        close_foods = [f[1] for f in foods if f[0] == min(food[0] for food in foods)]
+        self.next_food = close_foods[random.choice(range(close_foods.__len__()))]
+        # comment this out to forgo maze distance computation and use manhattan distances
+        # self.distancer.getMazeDistances()
 
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
-    """
-    self.distancer = distanceCalculator.Distancer(gameState.data.layout)
-    self.distancer.getMazeDistances()
-    #get_tunnels(gameState.data.layout)
-    Food = gameState.getFood()
-    self.foodlist = []
-    for h in range(Food.height):
-            for w in range(Food.width):
-              if Food[w][h]:
-                self.foodlist.append((w, h))
-    foods = [(self.distancer.getDistance(gameState.getPacmanPosition(), food),food) for food in self.foodlist]
-    closefoods = [f[1] for f in foods if f[0] == min(food[0] for food in foods)]
-    self.nextfood = closefoods[random.choice(range(closefoods.__len__()))]
-    # comment this out to forgo maze distance computation and use manhattan distances
-    # self.distancer.getMazeDistances()
+        import __main__
+        if '_display' in dir(__main__):
+            self.display = __main__._display
 
-    import __main__
-    if '_display' in dir(__main__):
-      self.display = __main__._display
+    #################
+    # Action Choice #
+    #################
 
+    def getAction(self, game_state):
+        """
+        Override this method to make a good agent. It should return a legal action within
+        the time limit (otherwise a random legal action will be chosen for you).
+        """
+        util.raiseNotDefined()
 
-  #################
-  # Action Choice #
-  #################
+    #######################
+    # Convenience Methods #
+    #######################
 
-  def getAction(self, gameState):
-    """
-    Override this method to make a good agent. It should return a legal action within
-    the time limit (otherwise a random legal action will be chosen for you).
-    """
-    util.raiseNotDefined()
+    def getFood(self, game_state):
+        """
+        Returns the food you're meant to eat. This is in the form of a matrix
+        where m[x][y]=true if there is food you can eat (based on your team) in that square.
+        """
+        return game_state.getFood()
 
-  #######################
-  # Convenience Methods #
-  #######################
+    def getCapsules(self, game_state):
+        return game_state.getCapsules()
 
-  def getFood(self, gameState):
-    """
-    Returns the food you're meant to eat. This is in the form of a matrix
-    where m[x][y]=true if there is food you can eat (based on your team) in that square.
-    """
-    return gameState.getFood()
+    def getScore(self, game_state):
+        """
+        Returns how much you are beating the other team by in the form of a number
+        that is the difference between your score and the opponents score.  This number
+        is negative if you're losing.
+        """
+        return game_state.getScore()
 
-  def getCapsules(self, gameState):
-    return gameState.getCapsules()
+    def getMazeDistance(self, pos1, pos2):
+        """
+        Returns the distance between two points; These are calculated using the provided
+        distancer object.
 
-
-  def getScore(self, gameState):
-    """
-    Returns how much you are beating the other team by in the form of a number
-    that is the difference between your score and the opponents score.  This number
-    is negative if you're losing.
-    """
-    return gameState.getScore()
-
-  def getMazeDistance(self, pos1, pos2):
-    """
-    Returns the distance between two points; These are calculated using the provided
-    distancer object.
-
-    If distancer.getMazeDistances() has been called, then maze distances are available.
-    Otherwise, this just returns Manhattan distance.
-    """
-    d = self.distancer.getDistance(pos1, pos2)
-    return d
+        If distancer.getMazeDistances() has been called, then maze distances are available.
+        Otherwise, this just returns Manhattan distance.
+        """
+        d = self.distancer.getDistance(pos1, pos2)
+        return d
 
 
 class BaselineAgent(CompetitionAgent):
-  """
-    This is a baseline reflex agent to see if you can do better.
-    A reflex agent chooses an action at each choice point by examining
-    its alternatives via a state evaluation function.
-  """
-
-  def getAction(self, gameState):
     """
-    getAction chooses among the best options according to the evaluation function.
-
-    Just like in the previous project, getAction takes a GameState and returns
-    some Directions.X for some X in the set {North, South, West, East, Stop}
+      This is a baseline reflex agent to see if you can do better.
+      A reflex agent chooses an action at each choice point by examining
+      its alternatives via a state evaluation function.
     """
 
-    # Collect legal moves and successor states
-    legalMoves = gameState.getLegalActions()
+    def getAction(self, game_state):
+        """
+        getAction chooses among the best options according to the evaluation function.
 
-    # try each of the actions and pick the best one
-    scores=[]
-    for action in legalMoves:
-      successorGameState = gameState.generatePacmanSuccessor(action)
-      scores.append(self.evaluationFunction(successorGameState))
+        Just like in the previous project, getAction takes a GameState and returns
+        some Directions.X for some X in the set {North, South, West, East, Stop}
+        """
 
-    # get the best action
-    bestScore = max(scores)
-    bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-    chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+        # Collect legal moves and successor states
+        legal_moves = game_state.getLegalActions()
 
-    "Add more of your code here if you want to"
-    return legalMoves[chosenIndex]
+        # try each of the actions and pick the best one
+        scores=[]
+        for action in legal_moves:
+            successor_game_state = game_state.generatePacmanSuccessor(action)
+            scores.append(self.evaluationFunction(successor_game_state))
 
-  def evaluationFunction(self, state):
-   # Useful information you can extract from a GameState (pacman.py)
-   return state.getScore()
+        # get the best action
+        best_score = max(scores)
+        best_indices = [index for index in range(len(scores)) if scores[index] == best_score]
+        chosen_index = random.choice(best_indices) # Pick randomly among the best
+
+        "Add more of your code here if you want to"
+        return legal_moves[chosen_index]
+
+    def evaluationFunction(self, state):
+        # Useful information you can extract from a GameState (pacman.py)
+        return state.getScore()
   
    
 class TimeoutAgent( Agent ):
@@ -172,14 +186,14 @@ class TimeoutAgent( Agent ):
     A random agent that takes too much time. Taking
     too much time results in penalties and random moves.
     """
-    def __init__( self, index=0 ):
+    def __init__(self, index=0):
         self.index = index
     
-    def getAction( self, state ):
-        import random, time
+    def getAction(self, state):
+        import random
+        import time
         time.sleep(2.0)
-        return random.choice( state.getLegalActions( self.index ) )
-	
+        return random.choice(state.getLegalActions(self.index))
 
 class MyPacmanAgent(CompetitionAgent):
     """
@@ -190,13 +204,13 @@ class MyPacmanAgent(CompetitionAgent):
     # The following functions have been declared for you,
     # but they don't do anything yet (getAction), or work very poorly (evaluationFunction)
 
-    def getAction(self, gameState):
+    def getAction(self, game_state):
         """
         getAction chooses among the best options according to the evaluation function.
         Just like in the previous projects, getAction takes a GameState and returns
         some Directions.X for some X in the set {North, South, West, East, Stop}.
         """
-        evaluation_scores = [(action, self.evaluationFunction2(gameState,gameState.generatePacmanSuccessor(action),action)) for action in gameState.getLegalActions(0)]
+        evaluation_scores = [(action, self.evaluationFunction2(game_state,game_state.generatePacmanSuccessor(action), action)) for action in game_state.getLegalActions(0)]
         best_actions = [action[0] for action in evaluation_scores if action[1] == max([score[1] for score in evaluation_scores])]
         return best_actions[random.choice(range(best_actions.__len__()))]
 
@@ -204,67 +218,66 @@ class MyPacmanAgent(CompetitionAgent):
         """
         A very poor evsaluation function. You can do better!
         """
-        currentGameState = state
-        Pos = currentGameState.getPacmanPosition()
-        Food = currentGameState.getFood()
-        GhostStates = currentGameState.getGhostStates()
-        ghostDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates if ghost.scaredTimer == 0]
-        scaredDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates if ghost.scaredTimer != 0]
-        capsuleDist = [self.distancer.getDistance(Pos, capsule)for capsule in currentGameState.getCapsules()]
-        foodlist = []
+        pacman_pos = state.getPacmanPosition()
+        food = state.getFood()
+        ghost_states = state.getGhostStates()
+        ghost_distance = [self.getDistance(pacman_pos, ghost.configuration.pos)for ghost in ghost_states if ghost.scaredTimer == 0]
+        scared_distance = [self.getDistance(pacman_pos, ghost.configuration.pos)for ghost in ghost_states if ghost.scaredTimer != 0]
+        capsule_distance = [self.getDistance(pacman_pos, capsule)for capsule in state.getCapsules()]
+        food_list = []
 
-        for h in range(Food.height):
-            for w in range(Food.width):
-               if Food[w][h]:
-                  foodlist.append((w,h))
-        foodDist = [self.distancer.getDistance(Pos, food)for food in foodlist]
-        if state.hasFood(Pos[0], Pos[1]):
-            foodDist.append(0)
-        if not ghostDistance:
-            ghostDistance.append(float("inf"))
+        for h in range(food.height):
+            for w in range(food.width):
+                if food[w][h]:
+                    food_list.append((w,h))
+        food_distance = [self.getDistance(pacman_pos, food)for food in food_list]
+        if state.hasFood(pacman_pos[0], pacman_pos[1]):
+            food_distance.append(0)
+        if not ghost_distance:
+            ghost_distance.append(float("inf"))
 
-        if min(ghostDistance) < 2:
+        if min(ghost_distance) < 2:
             return -float("inf")
-        if not foodDist:
+        if not food_distance:
             return float("inf")
-        return state.getScore() - min(foodDist)
+        return state.getScore() - min(food_distance)
 
     def evaluationFunction2(self, prevstate, state,action):
         """
         A very poor evsaluation function. You can do better!
         """
-        currentGameState = state
-        Pos = currentGameState.getPacmanPosition()
-        Food = currentGameState.getFood()
-        GhostStates = currentGameState.getGhostStates()
-        ghostDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates if ghost.scaredTimer == 0]
-        scaredDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates if ghost.scaredTimer != 0]
-        capsuleDist = [self.distancer.getDistance(Pos, capsule)for capsule in currentGameState.getCapsules()]
-        foodlist = []
+        currentgamestate = state
+        pacman_pos = currentgamestate.getPacmanPosition()
+        food = currentgamestate.getFood()
+        ghost_states = currentgamestate.getGhostStates()
+        ghost_distance = [self.distancer.getDistance(pacman_pos, ghost.configuration.pos)for ghost in ghost_states if ghost.scaredTimer == 0]
+        scared_distance = [self.distancer.getDistance(pacman_pos, ghost.configuration.pos)for ghost in ghost_states if ghost.scaredTimer != 0]
+        capsule_distance = [self.distancer.getDistance(pacman_pos, capsule)for capsule in currentgamestate.getCapsules()]
+        food_list = []
 
-        for h in range(Food.height):
-            for w in range(Food.width):
-               if Food[w][h]:
-                  foodlist.append((w,h))
-        foodDist = [self.distancer.getDistance(Pos, food)for food in foodlist]
-        if prevstate.getScore() < currentGameState.getScore():
-            foodDist.append(0)
-        if not ghostDistance:
-            ghostDistance.append(float("inf"))
-        if not capsuleDist or currentGameState.getCapsules().__len__() < prevstate.getCapsules().__len__():
-            capsuleDist.append(0)
-            scaredDistance.append(0)
-        #print ((scaredDistance,not scaredDistance))
-       # if scaredDistance:
-        #   capsuleDist = [-capsule for capsule in capsuleDist]
-        if not scaredDistance or scaredDistance.__len__() < [self.distancer.getDistance(prevstate.getPacmanPosition(), ghost.configuration.pos) for ghost in prevstate.getGhostStates() if ghost.scaredTimer != 0].__len__():
-            scaredDistance.append(0)
-        if min(ghostDistance) < 2:
+        for h in range(food.height):
+            for w in range(food.width):
+                if food[w][h]:
+                    food_list.append((w, h))
+        food_distance = [self.distancer.getDistance(pacman_pos, food)for food in food_list]
+        if prevstate.getScore() < currentgamestate.getScore():
+            food_distance.append(0)
+        if not ghost_distance:
+            ghost_distance.append(float("inf"))
+        if not capsule_distance or currentgamestate.getCapsules().__len__() < prevstate.getCapsules().__len__():
+            capsule_distance.append(0)
+            scared_distance.append(0)
+        # print ((scared_distance,not scared_distance))
+        # if scared_distance:
+        #   capsule_distance = [-capsule for capsule in capsule_distance]
+        if not scared_distance or scared_distance.__len__() < [self.distancer.getDistance(prevstate.getPacmanPosition(), ghost.configuration.pos) for ghost in prevstate.getGhostStates() if ghost.scaredTimer != 0].__len__():
+            scared_distance.append(0)
+        if min(ghost_distance) < 2:
             return -float("inf")
-        if not foodDist:
+        if not food_distance:
             return float("inf")
-        #print ((prevstate.getPacmanPosition(), - min(foodDist)  -10* min(capsuleDist) - 20*min(scaredDistance),action,(min(foodDist),min(capsuleDist),min(scaredDistance),state.getScore())))
-        return - min(foodDist)  - 10* min(capsuleDist) - 20*min(scaredDistance) + state.getScore()
+        #print ((prevstate.getPacmanPosition(), - min(food_distance)  -10* min(capsule_distance) - 20*min(scared_distance),action,(min(food_distance),min(capsule_distance),min(scared_distance),state.getScore())))
+        return - min(food_distance)  - 10* min(capsule_distance) - 20*min(scared_distance) + state.getScore()
 
 
 #MyPacmanAgent=BaselineAgent
