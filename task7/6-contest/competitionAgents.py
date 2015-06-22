fdee44e2093798a622945240936621fc96ca48fb
mattijn <m.kreuzen@student.ru.nl>
2015-06-23 00:12:20 +0200

    ingeleverde versie

--- task7/6-contest/competitionAgents.py
+++ task7/6-contest/competitionAgents.py
@@ -11,9 +11,14 @@ from game import Directions, Agent
 import random, util
 import distanceCalculator
 import math
-def get_tunnels(layout):
-    print (layout)
-    return
+class Corridor():
+    def __init__(self,layout):
+        self.layout = layout
+
+    def get_corridor(self,layout):
+        # print (layout.walls.data)
+        return
+
 class CompetitionAgent(Agent):
   """
   A base class for competition agents.  The convenience methods herein handle
@@ -54,14 +59,14 @@ class CompetitionAgent(Agent):
     """
     This method handles the initial setup of the
     agent to populate useful fields.
-    
-    A distanceCalculator instance caches the maze distances 
+
+    A distanceCalculator instance caches the maze distances
     between each pair of positions, so your agents can use:
     self.distancer.getDistance(p1, p2)
     """
     self.distancer = distanceCalculator.Distancer(gameState.data.layout)
     self.distancer.getMazeDistances()
-    get_tunnels(gameState.data.layout)
+    #get_tunnels(gameState.data.layout)
     Food = gameState.getFood()
     self.foodlist = []
     for h in range(Food.height):
@@ -73,7 +78,7 @@ class CompetitionAgent(Agent):
     self.nextfood = closefoods[random.choice(range(closefoods.__len__()))]
     # comment this out to forgo maze distance computation and use manhattan distances
     # self.distancer.getMazeDistances()
-    
+
     import __main__
     if '_display' in dir(__main__):
       self.display = __main__._display
@@ -139,7 +144,7 @@ class BaselineAgent(CompetitionAgent):
     Just like in the previous project, getAction takes a GameState and returns
     some Directions.X for some X in the set {North, South, West, East, Stop}
     """
-    
+
     # Collect legal moves and successor states
     legalMoves = gameState.getLegalActions()
 
@@ -148,7 +153,7 @@ class BaselineAgent(CompetitionAgent):
     for action in legalMoves:
       successorGameState = gameState.generatePacmanSuccessor(action)
       scores.append(self.evaluationFunction(successorGameState))
-    
+
     # get the best action
     bestScore = max(scores)
     bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
@@ -163,197 +168,103 @@ class BaselineAgent(CompetitionAgent):
   
    
 class TimeoutAgent( Agent ):
-  """
-  A random agent that takes too much time. Taking
-  too much time results in penalties and random moves.
-  """
-  def __init__( self, index=0 ):
-    self.index = index
+    """
+    A random agent that takes too much time. Taking
+    too much time results in penalties and random moves.
+    """
+    def __init__( self, index=0 ):
+        self.index = index
     
-  def getAction( self, state ):
-    import random, time
-    time.sleep(2.0)
-    return random.choice( state.getLegalActions( self.index ) )
+    def getAction( self, state ):
+        import random, time
+        time.sleep(2.0)
+        return random.choice( state.getLegalActions( self.index ) )
 	
 
 class MyPacmanAgent(CompetitionAgent):
-  """
-  This is going to be your brilliant competition agent.
-  You might want to copy code from BaselineAgent (above) and/or any previos assignment.
-  """
-
-  # The following functions have been declared for you,
-  # but they don't do anything yet (getAction), or work very poorly (evaluationFunction)
-
-  def getAction(self, gameState):
     """
-    getAction chooses among the best options according to the evaluation function.
-    Just like in the previous projects, getAction takes a GameState and returns
-    some Directions.X for some X in the set {North, South, West, East, Stop}. 
+    This is going to be your brilliant competition agent.
+    You might want to copy code from BaselineAgent (above) and/or any previos assignment.
     """
-    self.depth = 2
-    a = self.minimax(gameState, True, self.depth)
-    #if a[1] == Directions.STOP:
-    #    for action in gameState.getLegalActions(0):
-    #        if gameState.generateSuccessor(0,action).getScore() > gameState.getScore():
-    #            return action
-    #if a[1] == Directions.STOP:
-    #    return gameState.getLegalActions()[random.choice(range(gameState.getLegalActions().__len__()))]
-    #print (a)
-    return a[1]
 
-    GhostStates = gameState.getGhostStates()
-#    ghostDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates]
-    for action in gameState.getLegalActions(0):
-        if gameState.generateSuccessor(0,action).isWin():
-            return action
-    Food = gameState.getFood()
-    foodlist = []
-    for h in range(Food.height):
-            for w in range(Food.width):
-              if Food[w][h]:
-                foodlist.append((w, h))
-    foodDists = []
-    for action in gameState.getLegalActions(0):
-        successor = gameState.generateSuccessor(0,action)
-        Pos = successor.getPacmanPosition()
-        ghostDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates]
-        if min (ghostDistance) > 1 and action != Directions.STOP:
-            if Pos in foodlist:
-                foodDists.append((action, 0))
-            else:
-                foodDists.append((action, self.distancer.getDistance(Pos, self.nextfood)))
-    if foodDists == []:
-        return Directions.STOP
-    t = [a for a in foodDists if a[1] == min(Fod[1] for Fod in foodDists)]
-    i = random.choice(range(t.__len__()))
-    #print (t[i], t)
-    if t[i][1] == 0:
+    # The following functions have been declared for you,
+    # but they don't do anything yet (getAction), or work very poorly (evaluationFunction)
+
+    def getAction(self, gameState):
+        """
+        getAction chooses among the best options according to the evaluation function.
+        Just like in the previous projects, getAction takes a GameState and returns
+        some Directions.X for some X in the set {North, South, West, East, Stop}.
+        """
+        evaluation_scores = [(action, self.evaluationFunction2(gameState,gameState.generatePacmanSuccessor(action),action)) for action in gameState.getLegalActions(0)]
+        best_actions = [action[0] for action in evaluation_scores if action[1] == max([score[1] for score in evaluation_scores])]
+        return best_actions[random.choice(range(best_actions.__len__()))]
+
+    def evaluationFunction(self, state):
+        """
+        A very poor evsaluation function. You can do better!
+        """
+        currentGameState = state
+        Pos = currentGameState.getPacmanPosition()
+        Food = currentGameState.getFood()
+        GhostStates = currentGameState.getGhostStates()
+        ghostDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates if ghost.scaredTimer == 0]
+        scaredDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates if ghost.scaredTimer != 0]
+        capsuleDist = [self.distancer.getDistance(Pos, capsule)for capsule in currentGameState.getCapsules()]
+        foodlist = []
+
         for h in range(Food.height):
             for w in range(Food.width):
-              if Food[w][h]:
-                self.foodlist.append((w, h))
-        foods = [(self.distancer.getDistance(Pos, food),food) for food in foodlist]
-        closefoods = [f[1] for f in foods if f[0] == min(food[0] for food in foods)]
-        self.nextfood = closefoods[random.choice(range(closefoods.__len__()))]
-       # print(self.nextfood)
-    return t[i][0]
-    foodDist = [self.distancer.getDistance(Pos, food)for food in foodlist]
-    a = self.minimax(gameState, True, self.depth)
-    return a[1]
+               if Food[w][h]:
+                  foodlist.append((w,h))
+        foodDist = [self.distancer.getDistance(Pos, food)for food in foodlist]
+        if state.hasFood(Pos[0], Pos[1]):
+            foodDist.append(0)
+        if not ghostDistance:
+            ghostDistance.append(float("inf"))
 
-  def isTrapped (self, currentGameState):
-    pacman = currentGameState.getPacmanPosition()
-    walls_around_pacman  = 0
-    for x in range(3):
-      for y in range(3):
-        if currentGameState.getWalls()[pacman[0]-x + 1][pacman[1]- y + 1] and (x-1 ==0 or y-1 ==0):
-          walls_around_pacman += 1
-    if walls_around_pacman >= 3:
-      return True
-    else:
-      return False
+        if min(ghostDistance) < 2:
+            return -float("inf")
+        if not foodDist:
+            return float("inf")
+        return state.getScore() - min(foodDist)
 
+    def evaluationFunction2(self, prevstate, state,action):
+        """
+        A very poor evsaluation function. You can do better!
+        """
+        currentGameState = state
+        Pos = currentGameState.getPacmanPosition()
+        Food = currentGameState.getFood()
+        GhostStates = currentGameState.getGhostStates()
+        ghostDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates if ghost.scaredTimer == 0]
+        scaredDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates if ghost.scaredTimer != 0]
+        capsuleDist = [self.distancer.getDistance(Pos, capsule)for capsule in currentGameState.getCapsules()]
+        foodlist = []
 
-  def isTerminal (self,gameState):
-    return gameState.isWin() or gameState.isLose()
-
-
-  def getSenarios(self, actionslist):
-      if actionslist == []:
-        return [[]]
-      else:
-        list = self.getSenarios(actionslist[1:])
-      list3 = []
-      #i = 0
-      for a in actionslist[0]:
-        list2 = []
-        for l in list:
-          list2.append([])
-          for item in l:
-            list2[list2.__len__() - 1].append(item)
-        for i in range(list2.__len__()):
-          list2[i].append(a)
-        for item in list2:
-          list3.append(item)
-      return list3
-
-  def getSuccessor(self, gameState, actions):
-    successor = gameState
-    for action in actions:
-      successor = successor.generateSuccessor(action[1],action[0])
-      if self.isTerminal(successor):
-        return successor
-    return successor
-
-  def minimax(self, gameState , maxi, depth):
-        if self.isTerminal(gameState) or depth == 0:
-          return (self.evaluationFunction(gameState), Directions.STOP)
-        temp = []
-        childeren = util.PriorityQueue()
-        if maxi:
-          actions = gameState.getLegalActions(0)
-          for a in actions:
-              if not a == Directions.STOP:
-                succsorGamestate = gameState.generateSuccessor(0,a)
-                child = self.minimax(succsorGamestate, not maxi, depth - 1)
-                temp.append((child,a))
-                childeren.push((child[0], a), -child[0])
-        if not maxi:
-          actionslists = []
-          for i in range(gameState.getNumAgents() - 1):
-            actionsghost = []
-            for a in gameState.getLegalActions(i + 1):
-              actionsghost.append((a, i + 1))
-            actionslists.append(actionsghost)
-          actions = self.getSenarios(actionslists)
-          for a in actions:
-            succsorGamestate = self.getSuccessor(gameState, a)
-            child = self.minimax(succsorGamestate, not maxi, depth)
-            temp.append((child,a))
-            childeren.push((child[0], a), child[0])
-      #  if depth == self.depth:
-           # print (temp)
-        return childeren.pop()
-
-  def evaluationFunction(self, state):
-    """
-    A very poor evsaluation function. You can do better!
-    """
-   #
-    return state.getScore()
-    currentGameState = state
-    Pos = currentGameState.getPacmanPosition()
-    Food = currentGameState.getFood()
-    GhostStates = currentGameState.getGhostStates()
-
-    ghostDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates if ghost.scaredTimer == 0]
-    scaredDistance = [self.distancer.getDistance(Pos, ghost.configuration.pos)for ghost in GhostStates if ghost.scaredTimer != 0]
-    capsuleDist = [self.distancer.getDistance(Pos, capsule)for capsule in currentGameState.getCapsules()]
-    foodlist = []
-    for h in range(Food.height):
-        for w in range(Food.width):
-            if Food[w][h]:
-                foodlist.append((w,h))
-    foodDist = [self.distancer.getDistance(Pos, food)for food in foodlist]
-    if foodDist == []:
-        return float("Inf")
-    if currentGameState.isLose():
-        return -float("Inf")
-    if currentGameState.isWin():
-        return float("Inf")
-    #return currentGameState.getScore()
-  #  return -min(ghostDistance) - random.choice(range(4))
-    if self.isTrapped(currentGameState):
-      return -float("Inf")
-    if ghostDistance == [] and scaredDistance == []:
-        value = 1/(min(foodDist)+1)
-        return (value)*0.1 + random.choice(range(round(value)+1))
-    if ghostDistance == []:
-      return min(foodDist) - 2 * min(scaredDistance) + currentGameState.getScore() - 100*len(capsuleDist) + random.choice(range(20))
-    return -2* min(foodDist) + min(ghostDistance) + currentGameState.getScore() - 100*len(capsuleDist)  + random.choice(range(min(foodDist)))
-    value = min(ghostDistance)/(min(foodDist)+1)
-    return (value)*0.1 + random.choice(range(round(value)+1))
+        for h in range(Food.height):
+            for w in range(Food.width):
+               if Food[w][h]:
+                  foodlist.append((w,h))
+        foodDist = [self.distancer.getDistance(Pos, food)for food in foodlist]
+        if prevstate.getScore() < currentGameState.getScore():
+            foodDist.append(0)
+        if not ghostDistance:
+            ghostDistance.append(float("inf"))
+        if not capsuleDist or currentGameState.getCapsules().__len__() < prevstate.getCapsules().__len__():
+            capsuleDist.append(0)
+            scaredDistance.append(0)
+        #print ((scaredDistance,not scaredDistance))
+       # if scaredDistance:
+        #   capsuleDist = [-capsule for capsule in capsuleDist]
+        if not scaredDistance or scaredDistance.__len__() < [self.distancer.getDistance(prevstate.getPacmanPosition(), ghost.configuration.pos) for ghost in prevstate.getGhostStates() if ghost.scaredTimer != 0].__len__():
+            scaredDistance.append(0)
+        if min(ghostDistance) < 2:
+            return -float("inf")
+        if not foodDist:
+            return float("inf")
+        #print ((prevstate.getPacmanPosition(), - min(foodDist)  -10* min(capsuleDist) - 20*min(scaredDistance),action,(min(foodDist),min(capsuleDist),min(scaredDistance),state.getScore())))
+        return - min(foodDist)  - 10* min(capsuleDist) - 20*min(scaredDistance) + state.getScore()
 
 
 #MyPacmanAgent=BaselineAgent
--- task7/6-contest/leaderboard.py
+++ task7/6-contest/leaderboard.py
@@ -31,11 +31,11 @@ def getGroupName(file):
 def gamesStats(games):
    scores = [game.state.getScore() for game in games]
    timeouts = [game.state.getTimeout() for game in games]
-   moves  = [game.numMoves for game in games]
+   moves  = [max(1,game.numMoves) for game in games]
    times  = [games[i].totalAgentTimes[0]/float(moves[i]) for i in range(len(games))]
    wins = [game.state.isWin() for game in games]
-   winRate = wins.count(True)/ float(len(wins))
-   avescore = sum(scores) / float(len(scores))
+   winRate = wins.count(True)/ max(1,float(len(wins)))
+   avescore = sum(scores) / max(1,float(len(scores)))
    return (scores,timeouts,moves,times,wins,winRate,avescore)
 
 
@@ -44,13 +44,13 @@ def generateHtml(games, name=None):
       scores = []; timeouts=[]; moves=[]; times=[]; wins=[]; winRate=[]; avescore=[];
       for laygames in games: # average the per-layout averages
          (lscores,ltimeouts,lmoves,ltimes,lwins,lwinRate,lavescore)=gamesStats(laygames)
-         scores.append(   sum(lscores)  /float(len(lscores))   )
-         timeouts.append( sum(ltimeouts)/float(len(ltimeouts)) )
-         moves.append(    sum(lmoves)   /float(len(lmoves))    )
-         times.append(    sum(ltimes)   /float(len(ltimes))    )
-         wins.append(     sum(lwins)    /float(len(lwins))     )
+         scores.append(   sum(lscores)  /float(max(1,len(lscores)))   )
+         timeouts.append( sum(ltimeouts)/float(max(1,len(ltimeouts))) )
+         moves.append(    sum(lmoves)   /float(max(1,len(lmoves)))    )
+         times.append(    sum(ltimes)   /float(max(1,len(ltimes)))    )
+         wins.append(     sum(lwins)    /float(max(1,len(lwins)))     )
          avescore.append(lavescore)
-      avescore = sum(avescore) / float(len(avescore))
+      avescore = sum(avescore) / float(max(1,len(avescore)))
    else:
       (scores,timeouts,moves,times,wins,winRate,avescore)=gamesStats(games)
 
@@ -73,46 +73,66 @@ def generateHtml(games, name=None):
    return (avescore,htmlout)
 
 def runFile(file,replay=False,args=[]):
+  failed = True
+  score  = 0  
+  htmlsummary= ''
   fileoutput = ''
+  games=None
   # Fetch student number
   print(file)
   studentNumber = getGroupName(file)
   if studentNumber is None: studentNumber=''
-  studentNumber=studentNumber.replace('20',' ')
-  print('Trying ' + studentNumber) 
+  print('Trying ' + studentNumber + '\nfrom file: ' + file) 
   fileoutput += studentNumber + ':' + '\n'
   #keyboard()
 
-  
   # Move their competitionAgents.py to the environment
-  try: 
-    shutil.copy(file,solutionFile)
-  except: 
-    pass
+  if not file == solutionFile :
+    try:
+      shutil.copyfile(file,solutionFile)
+    except shutil.SameFileError as err: # samefile isn't a bug
+      pass
+    except IOError as err:
+      print('Error couldnt copy over the solution file: ' + file)
+      return (score,htmlsummary)
 
   # Run the environment
   output = ''
   failed = False
-  try:
-    print('Trying ' + studentNumber)
-    # force reload of student code -- needed even for load to get the class definitions correctly
-    imp.reload(sys.modules[solutionModule]) 
-    if os.path.isfile(studentNumber + '_games.pk') and not replay:
-      games = loadobject(studentNumber + '_games.pk')
-    else:
-      games = pacman.cmdlineRunGames(execList + args)
-      saveobject(studentNumber + '_games.pk',games) # save so don't have to re-run later
-    
-    score,htmlsummary = generateHtml(games,studentNumber)    
 
-  except subprocess.CalledProcessError as e:
-    failed = True
-    output = e.output
+  # force reload of student code -- needed even for load to get the class definitions correctly
+  try :
+    print(sys.modules[solutionModule])
+    imp.reload(sys.modules[solutionModule]) 
+  except:
+    failed=True
+    output = 'Error could not load solution module';
+    print(output)
+    
+  if not failed :
+      if os.path.isfile(studentNumber + '_games.pk') and not replay:
+        games = loadobject(studentNumber + '_games.pk')
+      else:
+        try:
+          nargs = execList;
+          print('Running with arguments :' + '  '.join(execList + args) )
+          games = pacman.cmdlineRunGames(execList + args)
+        except subprocess.CalledProcessError as e:          
+          failed = True
+          output = e.output
+      if not failed:
+        try:
+          saveobject(studentNumber + '_games.pk',games) # save so don't have to re-run later
+        except:
+          print('Couldnt pickel the saved games...')
+        score,htmlsummary = generateHtml(games,studentNumber)    
+
   # Print result
   if failed:
     fileoutput += 'Failed :\n' + output + '\n\n'
   else:
     fileoutput += 'Output :\n' + output + '\n\n'
+  print(fileoutput)
   return (score,htmlsummary)
 
 def help():
@@ -125,7 +145,7 @@ If no studentNumber argument is given, it will run all pairs of search.py and se
 
 -h|--help\t\tDisplays this help text.
 --dir=[directory]\tSets the default directory with student solutions to the entered one. By default it is `{defaultDir}`.
---num=[studentNumber]\tScript will only execute search.py and searchAgents.py from this student.
+--num=[studentNumber]\tScript will only execute search.py and searchAgents.py from this student group.
 
 Examples:
 ./{filename}\t\t\tRuns all file pairs in {defaultDir}/
@@ -136,15 +156,22 @@ Examples:
 def main(argv):
   defaultDirectory = 'studentsolutions'
   studentNumber = None
+  replay=False
+  args=None
+
+  # Remove the old fileoutput file
+  if os.path.isfile(logfileName):
+     shutil.copy(logfileName,logfileName + '.bak')
+     os.remove(logfileName)
   logfile = open(logfileName,'w');
 
   # Parse the arguments
+  opts=[]
   try:
      opts, args = getopt.getopt(argv, 'h', ['help', 'dir=', 'num=', 'replay'])
   except getopt.GetoptError:
-    opts=[]
     _, e, _ = sys.exc_info()
-    if e.msg.find('not recognized')<0: # pass through unrecog optoins
+    if e.msg.find('not recognized')<0: # pass through unrecog options
        print((help()))
        sys.exit(2)
 
@@ -157,15 +184,9 @@ def main(argv):
     elif opt == '--num':
        studentNumber = arg
     elif opt == '--replay':
-       replay = True
-       
-  # Remove the old fileoutput file
-  if os.path.isfile('output.txt'):
-     try:
-       os.remove('output.txt')
-     except:
-       pass
-   
+       replay = True       
+
+  print(args)
   fileoutput = ''
 
   if not os.path.isdir(defaultDirectory):
@@ -179,29 +200,35 @@ def main(argv):
     # extract student number and remove duplicates
     oldgroup=None
     files=[]
-    #print '\n'.join(solnfiles)
-    for file in solnfiles:
-      groupName = getGroupName(file)
-      if not groupName == None :
-        if groupName == oldgroup:
-          files[-1]=file
-        else:
-          oldgroup = groupName
-          files.append(file)
-    #print '\n'.join(files)
+    #print('\n'.join(solnfiles))
+    if len(solnfiles)==1:
+      files=solnfiles
+    else:
+      for file in solnfiles:
+        groupName = getGroupName(file)
+        if not groupName == None :
+          if groupName == oldgroup:
+            files[-1]=file
+          else:
+            oldgroup = groupName
+            files.append(file)
+    print('\n'.join(files))
     for file in files:
       score,output = runFile(file,replay,args)
+      print(output)
       logfile.write(output)
       info.append((score,output))
   else:
     # If you want to run one at the time using the student number
     for file in glob.glob(defaultDirectory + '/*' + solutionFile):
       groupName = getGroupName(file)
-      if studentNumber == groupName or ( groupName is None and studentNumber=='' ):
+      if 'Groep ' + studentNumber == groupName or ( groupName is None and studentNumber=='' ):
         score,output = runFile(file,replay,args)
-    logfile.write(output)
-    info.append((score,output))
+        print(output)
+        logfile.write(output)
+        info.append((score,output))
 
+  logfile.close()
   # sort the results based on score
   info = sorted(info,reverse=True)
   shutil.copy(htmlstyle,htmloutputfile) # copy in style info
@@ -215,25 +242,31 @@ def main(argv):
      hfile.write(output)
   # write the footer
   hfile.write("</table>");
+  hfile.close()
   return info
 
 if __name__ == "__main__":
-  # make back up of current solution file, if it's not already there!
+  # make back up of current solution file
+  if os.path.isfile(solutionFile):
+    shutil.copy(solutionFile,solutionFile+ ".bak");
   if not os.path.isfile(solutionFile+".orig"):
     shutil.copy(solutionFile,solutionFile + ".orig")  
   try:
     __import__(solutionModule)
-  except: # current solutinFile is broken, copy back the .orig 
-    print("Warning: " + solutionFile + " is broken!  reverting to " + solutionFile + ".orig")
-    print("     your broken one is saved to : " + solutionFile + ".bak")
-    shutil.copy(solutionFile,solutionFile + ".bak") # restore backup    
-    shutil.copy(solutionFile + ".orig",solutionFile) # restore backup
-    __import__(solutionModule)
-    
+  except :
+    print("Couldnt import the file " + solutionFile)
+    print("copy back from orginal and re-try:" + solutionFile + ".orig")
+    sys.exit(-1)
+      
   # ask for arguments if none given
   if len(sys.argv) == 1:
     if sys.version_info < (3,0):
+      print("Python version <3.0 detected, YMMV")
       sys.argv.extend([x for x in re.split(r' *',raw_input("Enter any command line arguments?")) if x!=''])
     else:
       sys.argv.extend([x for x in re.split(r' *',input("Enter any command line arguments?")) if x!=''])
   main(sys.argv[1:])
+
+  # copy the orginal file back
+  #if os.path.isfile(solutionFile + '.orig'):
+  #  shutil.copy(solutionFile + ".orig",solutionFile) # restore backup
